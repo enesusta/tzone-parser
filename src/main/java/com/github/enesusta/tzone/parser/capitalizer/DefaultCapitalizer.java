fb67363cfa2252156f052a03705c6acf6c9d1481
enesusta <enesusta@email.com>
2020-10-17 11:30:27 +0300

    Remove parenthesis on cell value

--- src/main/java/com/github/enesusta/tzone/parser/capitalizer/DefaultCapitalizer.java
+++ src/main/java/com/github/enesusta/tzone/parser/capitalizer/DefaultCapitalizer.java
@@ -2,7 +2,6 @@ package com.github.enesusta.tzone.parser.capitalizer;
 
 import com.github.enesusta.tzone.parser.util.StringUtils;
 
-import java.util.Arrays;
 
 public class DefaultCapitalizer implements Capitalizer {
 
@@ -10,32 +9,26 @@ public class DefaultCapitalizer implements Capitalizer {
     public String capitalize(String cell) {
         final StringBuilder stringBuilder = new StringBuilder();
         boolean hasSpace = false;
-        boolean hasParenthesis = false;
-        boolean hasDot = true;
         for (byte aByte : cell.getBytes()) if (aByte == 32) hasSpace = true;
-        for (byte b : cell.getBytes()) if (b == 40) hasParenthesis = true;
 
         if (hasSpace) {
             String res = removeParenthesis(cell);
             String[] split = res.split("\\s");
             for (String s : split) {
                 if (s.equalsIgnoreCase("MAH")) {
-                    hasDot = false;
                     String capitalized = StringUtils.capitalizeWithTurkish(s);
                     stringBuilder.append(capitalized);
-                    stringBuilder.append('.');
+                    stringBuilder.append("allesi");
                 } else {
                     stringBuilder.append(StringUtils.capitalizeWithTurkish(s));
                 }
                 stringBuilder.append(' ');
             }
-            if (hasDot)
-                stringBuilder.setCharAt(stringBuilder.length() - 1, '.');
+            stringBuilder.setCharAt(stringBuilder.length() - 1, '.');
         } else {
             stringBuilder.append(cell);
         }
 
-        //  System.out.println(stringBuilder.toString());
         return stringBuilder.toString();
     }
 
